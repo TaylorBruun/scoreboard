6e52ace1b4244a8e556cefb096c9c6c123ee257e
TaylorBruun <taylorbruun@gmail.com>
2022-05-16 16:18:17 -0600

    removed redundant code, improved annotations

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 let p1Score = 0
 let p2Score = 0
 
-// switch version - very broken
-// function addP1Score(){
-//     switch (p1Score) {
-//         case p1Score == 0 || p1Score == 15: p1Score += 15;
-//         break;
-//         case p1Score == 30: p1Score += 10;
-//         break;
-//         case p1Score = p2Score: p1Score = "Ad", p2Score = "--";
-//         break;
-//         case p1Score = Ad || (p1Score = 40 && p2Score !=40): p1Score = "Win!";
-//         break;
-//         case p1Score = "--": p1Score = "Ad";
-//         break;
-//         }
-//     drawP1Score()
-// }
 
+// NOTE I forgot how deuce/advantage works so the scoreboard is working exactly as intended, but my intent was incorrect.
 
 // nested else...if
 function addP1Score() {
@@ -36,20 +21,16 @@ function addP1Score() {
     else if (((p1Score == 40) && (p2Score == 40)) || (p1Score == "--")) {
         p1Score = "Ad"
         p2Score = "--"
-        console.log("condition 4")
-        console.log("p1Score is ", p1Score)
-        console.log("p2Score is ", p2Score)
-
-        console.log((((p1Score == 40) && (p2Score == 40)) || (p1Score == "--")))
     }
+       
     else if (((p1Score == 40) && (p2Score !== 40)) || (p1Score == "Ad")) {
         p1Score = "Win!"
         console.log("condition 5")
     }
     else {
-        p1Score == ":(" 
+        p1Score == ":("
         console.log("condition 6")
-        
+
     }
     drawP1Score()
     drawP2Score()
@@ -74,40 +55,41 @@ function addP2Score() {
         p2Score = "Win!"
     }
     else {
-        p2Score == ":(" 
-        
+        p2Score == ":("
+
     }
     drawP1Score()
     drawP2Score()
 }
 
-    function subP1Score(){
-        if(p1Score == 30 || p1Score == 15){
-            p1Score -= 15
-        }
-            else if(p1Score == 40){
-                p1Score -= 10
-            }
-            else{
-                p1Score = 0
-            }
-            drawP1Score()   
+function subP1Score() {
+    if (p1Score == 30 || p1Score == 15) {
+        p1Score -= 15
+    }
+    else if (p1Score == 40) {
+        p1Score -= 10
     }
 
-    function subP2Score(){
-        if(p2Score == 30 || p2Score == 15){
-            p2Score -= 15
-        }
-            else if(p2Score == 40){
-                p2Score -= 10
-            }
-            else{
-                p2Score = 0
-            }
-            drawP2Score()   
+    else {
+        p1Score = 0
     }
+    drawP1Score()
+}
 
-function reset(){
+function subP2Score() {
+    if (p2Score == 30 || p2Score == 15) {
+        p2Score -= 15
+    }
+    else if (p2Score == 40) {
+        p2Score -= 10
+    }
+    else {
+        p2Score = 0
+    }
+    drawP2Score()
+}
+
+function reset() {
     p1Score = 0
     p2Score = 0
     drawP1Score()
@@ -119,10 +101,10 @@ function reset(){
 //     console.log("you clicked button", identifier)
 // }
 
-function drawP1Score(){
+function drawP1Score() {
     document.getElementById('scoreP1').innerHTML = p1Score
 }
-function drawP2Score(){
+function drawP2Score() {
     document.getElementById('scoreP2').innerHTML = p2Score
 }
 
